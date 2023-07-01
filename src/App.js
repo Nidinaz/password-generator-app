2c5adee478af628ce9384e7fa8be5c768a0cfad1
Dorina Kamp <dorina.kamp@gmail.com>
2023-07-01 12:36:14 +0200

    button and generated password

--- src/App.js
+++ src/App.js
@@ -1,11 +1,36 @@
-import './App.css';
+import "./App.css";
+import React, { useState } from "react";
 
-function App() {
+const App = () => {
+  //useState to change password and save the new state
+  const [password, setPassword] = useState("");
+
+  const generatePassword = () => {
+
+    //characters define the items used to
+    const characters =
+      "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*";
+
+    //emty string where the new password is made
+    //charAt function to select a random character from a string
+    //Math.random() produces a random number
+    //and characters.length
+    //Math.floor makes sure that it's a whole number
+    let newPassword = "";
+    for (let i = 0; i < 12; i++) {
+      newPassword += characters.charAt(
+        Math.floor(Math.random() * characters.length)
+      );
+    }
+    setPassword(newPassword);
+  };
+
   return (
-    <div className="App">
-
+    <div>
+      <button onClick={generatePassword}>Generate Password</button>
+      <p>{password}</p>
     </div>
   );
-}
+};
 
 export default App;
